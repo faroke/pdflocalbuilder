a4ff5994c97802255b076882aa2b5dec4e532d4a
yann <yann.fo7@gmail.com>
2022-10-13 16:27:09 +0200

    init

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.locals.pretty = true;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/:template_id", async (req, res) => {
+app.post("/build/:template_id", async (req, res) => {
     const {template_id} = req.params
     const path = `/tmp/${template_id}.pdf`;
     try {
         const item = await getById(template_id)
-        console.log(req.body)
         const twigRender = await twig.build(item.source, req.body)
         await pdfToHtml.converter(twigRender, path);
         if (req.query.hasOwnProperty('encode') && req.query.encode === 'base64') {
@@ -42,7 +41,82 @@ app.post("/:template_id", async (req, res) => {
         return res.status(500).send(e);
     }
 });
-
+app.get("/html/:template_id", async (req, res) => {
+    app.set('view engine', 'html');
+    const {template_id} = req.params
+    try {
+        const item = await getById(template_id)
+        const twigRender = await twig.build(item.source,{
+            "start_date": "31/01/2022 à 21h30",
+            "end_date": "31/10/2022 à 21h30",
+            "displayName": "toto",
+            "displayRenterName": "Nom du loueur",
+            "siren" : "000000000",
+            "address": "2 rue des champs",
+            "phone_number": "0606060606",
+            "email" : "monmail@gmail.com",
+            "company_name": "Nom d'entreprise",
+            "usage": "Privé - trajet travail / Professionnel / Transports",
+            "sharing" : "Partagé",
+            "created_date" : "31/02/2021",
+            "build_date": "30/02/2021",
+            "total_amount" : "200 €",
+            "tax" : "20 €",
+            "catnat": "200 € dont 20 € de taxes",
+            "products" : [
+                {
+                    "brand": "Marque vélo 1",
+                    "model": "Marque vélo 1",
+                    "marked" : "Numéro de gravage du vélo 1",
+                    "price": "Valeur d'achat à neuf vélo 1"
+                },
+                {
+                    "brand": "Marque vélo 2",
+                    "model": "Marque vélo 2",
+                    "marked" : "Numéro de gravage du vélo 2",
+                    "price": "Valeur d'achat à neuf vélo 2"
+                },
+                {
+                    "brand": "Marque vélo 3",
+                    "model": "Marque vélo 3",
+                    "marked" : "Numéro de gravage du vélo 3",
+                    "price": "Valeur d'achat à neuf vélo 3"
+                }
+            ],
+            "receipts": [
+                {
+                    "due_date": "31/02/2021",
+                    "start_date" : "31/02/2021",
+                    "end_date": "31/02/2021",
+                    "amount" : "400 €"
+                },
+                {
+                    "due_date": "31/02/2021",
+                    "start_date" : "31/02/2021",
+                    "end_date": "31/02/2021",
+                    "amount" : "400 €"
+                },
+                {
+                    "due_date": "31/02/2021",
+                    "start_date" : "31/02/2021",
+                    "end_date": "31/02/2021",
+                    "amount" : "400 €"
+                }
+            ],
+            "theft": false,
+            "break": true,
+            "natural_disaster": true,
+            "technological_disaster": true,
+            "ia": false,
+            "rc": true,
+            "ia_itt": "5 000€",
+            "ia_death": "400 000€"
+        })
+        res.send(twigRender)
+    } catch (e) {
+        return res.status(500).send(e);
+    }
+})
 app.listen(8080, () => {
     console.log(`App listening at http://localhost:8080`);
 });
--- utils/pdfToHtml.js
+++ utils/pdfToHtml.js
@@ -8,12 +8,26 @@ export async function converter(html, path) {
         await page.emulateMediaType('screen');
         await page.pdf({
             path: path,
-            margin: {top: '100px', right: '50px', bottom: '100px', left: '50px'},
+            //margin: {top: '100px', right: '50px', bottom: '100px', left: '50px'},
             printBackground: true,
             format: 'A4',
             displayHeaderFooter: true,
-            headerTemplate: "<div><div class='pageNumber'></div> <div>/</div><div class='totalPages'></div></div>",
-            footerTemplate: "<div style=\"text-align: right;width: 297mm;font-size: 8px;\"><span style=\"margin-right: 1cm\"><span class=\"pageNumber\"></span> of <span class=\"totalPages\"></span></span></div>"
+            headerTemplate: "<div></div>",
+            footerTemplate: "<div style='width: 100%; display: flex;flex-direction: row; justify-content: space-between'" +
+                "><div " +
+                "style=\"font-size:11px;margin-left: 1cm\">" +
+                "Contrat n° XXXXXXX</div>" +
+                "<div " +
+                "style=\"text-align: right;font-size: 11px;\"" +
+                "><span style=\"margin-right: 1cm\">" +
+                "<span class=\"pageNumber\"></span> / <span class=\"totalPages\"></span></span></div></div>",
+            //footerTemplate: '<div style="font-size:10px !important; color:#808080;margin-right: 1cm; text-align: right"><span style="margin-right: 1cm"><span class="pageNumber"></span> / <span class="totalPages"></span></span></div>',
+            margin: {
+                top: '1.27cm',
+                bottom: '135px',
+                right: '1.27cm',
+                left: '1.27cm',
+            },
         })
         await browser.close()
         return true
